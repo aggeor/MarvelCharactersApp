d1da895d3a87b800fcb2f36d85b3889c3c046604
aggeor <ag.gewr@gmail.com>
2024-05-12 13:12:38 +0300

    Initial Commit

---
+++ MarvelCharacters/MarvelCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelCharactersApp.swift
+//  MarvelCharacters
+//
+//  Created by Aggelos Geo on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
