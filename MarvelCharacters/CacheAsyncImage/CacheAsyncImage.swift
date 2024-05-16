5fc5f6c519aee845fb0294bf04a35e216b3218fb
aggeor <ag.gewr@gmail.com>
2024-05-16 18:42:58 +0300

    add image caching

---
+++ MarvelCharacters/CacheAsyncImage/CacheAsyncImage.swift
@@ -0,0 +1,18 @@
+//
+//  CacheAsyncImage.swift
+//  MarvelCharacters
+//
+//  Created by Aggelos Geo on 16/5/24.
+//
+
+import SwiftUI
+
+struct CacheAsyncImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CacheAsyncImage()
+}
---
+++ MarvelCharacters/CacheAsyncImage/NetworkImage.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkImage.swift
+//  MarvelCharacters
+//
+//  Created by Aggelos Geo on 16/5/24.
+//
+
+import SwiftUI
+
+struct NetworkImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkImage()
+}
